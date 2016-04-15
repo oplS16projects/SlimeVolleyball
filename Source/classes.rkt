bd77592ade4af952f870b3ba9fe7cdb37eb7e095
C4m0 <cameron_oliver@student.uml.edu>
2016-04-15 14:27:46 -0400

    Spring Cleaning
    Combined all the separate bits of code, fixed it up so everything works together and cleaned the crap out of the repo!

---
+++ Source/classes.rkt
@@ -0,0 +1,58 @@
+#lang racket
+(require racket/gui/base)
+;this provide condition makes it so you can require this file
+;in anther file and all of these definitions will be usable.
+(provide (all-defined-out))
+
+;for boundary checking
+(define windowXbound 1000)
+(define windowYbound 500)
+(define slimebot (- windowYbound 68))
+(define bitmap1 (make-object bitmap% 100 100))
+(send bitmap1 load-file "greenslime.png")
+(define bitmap2 (make-object bitmap% 100 100))
+(send bitmap2 load-file "redslime.png")
+(define dx 1000) ;temporary
+(define dy 1000) ;temporary
+
+;left bound and right bound have been added to make it easier to restrict each player to half
+;the screen
+(define (make_object pos_x pos_y vel_x vel_y radius left_bound right_bound bottom_bound)
+  (define (dispatch op)
+    (cond ((eq? op 'get_pos) (cons pos_x pos_y))
+          ((eq? op 'get_vel) (cons vel_x vel_y))
+          ((eq? op 'get_rad) radius)
+          ((eq? op 'set_pos) (lambda (x y)
+                               (if (and (> x left_bound) (< x right_bound))
+                                   (set! pos_x x)
+                                   (values));does nothing
+                               (if (and (> y 0) (< y bottom_bound))
+                                   (set! pos_y y)
+                                   (values))))
+          ; (if (< y windowYbound) (set! pos_y windowYbound) (values )))))
+          ((eq? op 'set_vel) (lambda (x y)
+                               (set! vel_x x)
+                               (set! vel_y y)))
+          ((eq? op 'move) (begin
+                            ((lambda (x y)
+                               (if (and (> x left_bound) (< x right_bound))
+                                   (set! pos_x x)
+                                   (values));does nothing
+                               (if  (< y bottom_bound)
+                                   (set! pos_y y)
+                                   (if (>= y bottom_bound) (set! pos_y bottom_bound) (values ))))
+                             (+ pos_x vel_x) (+ pos_y vel_y)))
+                          (if (< pos_y bottom_bound) (set! vel_y (+ vel_y 2)) (set! vel_y 0))) ;gravity
+          ((eq? op 'collision) (lambda (Slime1)  (if (<= (sqrt (+ (expt (- (+ (car (Slime1 'get_pos)) (Slime1 'get_rad)) (+ pos_x radius)) 2) (expt (- (+ (cdr (Slime1 'get_pos)) (Slime1 'get_rad)) (+ pos_y radius)) 2))) (+ radius (Slime1 'get_rad)))
+                                               (set! vel_y -25)
+                                               #f)))
+    (else (error "Unknown op: " op))))
+    dispatch)
+
+;defines ball as an object with a radius of 30 px and starting position above player 1
+(define ball (make_object (/ windowXbound 4) (/ windowYbound 4) 0 0 18 0 windowXbound (- windowYbound 36)))
+;slimes are defined as circular objects with radius 100, they are placed at the bottom of the viewing window,
+;so the bottom half of the sphere gets clipped (this means when you jump there is actually a circle moving
+;rather than a half circle, but the ball will never hit the lower half of the circle and this is easier to implement.
+(define Slime1 (make_object (/ windowXbound 4) (- windowYbound 200) 0 0 78 0 (- (/ windowXbound 2) 138) slimebot))
+(define Slime2 (make_object (* 3(/ windowXbound 4)) windowYbound 0 0 78 (+ (/ windowXbound 2) 2) (- windowXbound 136) slimebot))
